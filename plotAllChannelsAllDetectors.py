2eb3864d002e42177908f2018c6d1538b3d8fb11
Adam Cox <adam.cox@kit.edu>
2011-12-08 11:28:08 +0100

    updating the plotAllChannels script to actually work. an example result root file is added to the repository. I haven't analyzed it yet

--- plotAllChannelsAllDetectors.py
+++ plotAllChannelsAllDetectors.py
@@ -2,17 +2,21 @@
 
 from ROOT import *
 from couchdbkit import Server, Database
-import sys, os, string, math
+import sys, os, string, math, json
 
 def getHist(name, amin, amax):  
-  h = TH1D(name,name,5000,amin,amax)
+  print name,name,500,amin,amax
+  h = TH1D(name,name,500,amin,amax)
   return h
 
 def get2dHist(name, xmin, xmax, ymin, ymax):
-  h = TH2D(name,name,5000,xmin,xmax,5000,ymin,ymax)
+  print name,name,100,xmin,xmax,100,ymin,ymax
+  h = TH2D(name,name,100,xmin,xmax,100,ymin,ymax)
   return h
+
+
   
-def main(arg):  
+def main(*arg):  
   
   gROOT.SetBatch(True)
   startRunName = arg[0]
@@ -42,28 +46,30 @@ def main(arg):
       f.GetEntry(i)
       
       for j in range(event.GetNumBolos()):
-        bolo = e.GetBolo(j)
+        bolo = event.GetBolo(j)
         detname = bolo.GetDetectorName()
         if detectorInfo.has_key(detname) == False:
           detectorInfo[detname] = {}
-          detectorInfo['maxSumIon'] = {}
-          detectorInfo['minSumIon'] = {}
-        
+          detectorInfo[detname]['maxSumIon'] = 0
+          detectorInfo[detname]['minSumIon'] = 0
+          detectorInfo[detname]['chans'] = {}
         sumIon = 0
           
         for k in range(bolo.GetNumPulseRecords()):
           pulse = bolo.GetPulseRecord(k)
           channame = pulse.GetChannelName()
+          if pulse.GetPulseLength() == 0:
+            continue  #skip events with zero pulse lenght
+
+          if detectorInfo[detname]['chans'].has_key(channame) == False:
+            detectorInfo[detname]['chans'][channame] = {}
+            detectorInfo[detname]['chans'][channame]['min'] = 0
+            detectorInfo[detname]['chans'][channame]['max'] = 0
+            detectorInfo[detname]['chans'][channame]['pulselength'] = pulse.GetPulseLength()
           
-          if detectorInfo[detname].has_key(channame) == False:
-            detectorInfo[detname][channame] = {}
-            detectorInfo[detname][channame]['min'] = 0
-            detectorInfo[detname][channame]['max'] = 0
-            detectorInfo[detname][channame]['pulselength'] = pulse.GetPulseLength()
-          
-          chanInfo = detectorInfo[detname][channame]
+          chanInfo = detectorInfo[detname]['chans'][channame]
           result = pulse.GetPulseAnalysisRecord(resultName)
-          polarity = polCalc.GetExpectedPolarity(p)
+          polarity = polCalc.GetExpectedPolarity(pulse)
             
           if result.IsBaseline() == 0:    
             if pulse.GetIsHeatPulse() == False:
@@ -72,28 +78,43 @@ def main(arg):
             if result.GetAmp() > chanInfo['max']: chanInfo['max'] = result.GetAmp()
             if result.GetAmp() < chanInfo['min']: chanInfo['min'] = result.GetAmp()
 
-        if sumIon > detectorInfo['maxSumIon']:  detectorInfo['maxSumIon'] = sumIon
-        if sumIon < detectorInfo['minSumIon']:  detectorInfo['minSumIon'] = sumIon
+        if sumIon > detectorInfo[detname]['maxSumIon']:  detectorInfo[detname]['maxSumIon'] = sumIon
+        if sumIon < detectorInfo[detname]['minSumIon']:  detectorInfo[detname]['minSumIon'] = sumIon
 
   #for each detector and channel, set up the histograms
+  fout = TFile(arg[3],'recreate')
   histList = []
   for det in detectorInfo.iterkeys():
-    detectorInfo[det]['sumIonHist'] = getHist(det+'_sumIon', detectorInfo['minSumIon'], detectorInfo['maxSumIon'])
+    print det
+    print json.dumps(detectorInfo[det], indent=1)
+
+    detectorInfo[det]['sumIonHist'] = 'test'
+    detectorInfo[det]['sumIonHist'] = getHist(det+'_sumIon', detectorInfo[detname]['minSumIon'], detectorInfo[detname]['maxSumIon'])
     histList.append(detectorInfo[det]['sumIonHist'])
     
-    for chan in detectorInfo[det].iterkeys():
-      chanInfo = detectorInfo[det][chan]
+    chans = detectorInfo[det]['chans']
+    for chan in chans.iterkeys():
+      print chan
+      chanInfo = chans[chan]
+      #print json.dumps(chanInfo, indent=1)
       chanInfo['hist'] = getHist(string.replace(chan, ' ', '_')+'_hist', chanInfo['min'], chanInfo['max'])
       histList.append(chanInfo['hist'])
       
       chanInfo['corrhists'] = {}
-      for otherChan in detectorInfo[det].iterkeys():
+      for otherChan in chans.iterkeys():
         if otherChan != chan:
+          print '     ', otherChan
+          print '       min', chans[otherChan]['min']
+          print '       max', chans[otherChan]['max']
+          #print '     ', json.dumps(chans[otherChan], indent=1)
           chanInfo['corrhists'][otherChan] = get2dHist(string.replace(chan, ' ', '_')+string.replace(otherChan, ' ', '_')+'_hist', 
-                                                      chanInfo['min'], chanInfo['max'], detectorInfo[det][otherChan]['min'], 
-                                                      detectorInfo[det][otherChan]['max'])
+                                                      chanInfo['min'], chanInfo['max'], chans[otherChan]['min'], 
+                                                      chans[otherChan]['max'])
           histList.append(chanInfo['corrhists'][otherChan])
                
+
+  for i in range(len(histList)):
+    print histList[i].GetName()
   
   #all of the histograms are now set up... loop back through the data and fill them in.
   for afile in fileList:
@@ -107,7 +128,7 @@ def main(arg):
       f.GetEntry(i)
       
       for j in range(event.GetNumBolos()):
-        bolo = e.GetBolo(j)
+        bolo = event.GetBolo(j)
         detname = bolo.GetDetectorName()
         
         
@@ -117,52 +138,83 @@ def main(arg):
         pulseWithMaxAmp = -1
         for k in range(bolo.GetNumPulseRecords()):
           pulse = bolo.GetPulseRecord(k)
-          
-          chanInfo = detectorInfo[detname][pulse.GetChannelName()]
+          if pulse.GetPulseLength() == 0:
+            #print 'pulse length is zero'
+            continue 
+
+          chanInfo = detectorInfo[detname]['chans'][pulse.GetChannelName()]
           result = pulse.GetPulseAnalysisRecord(resultName)
-          polarity = polCalc.GetExpectedPolarity(p)
+          polarity = polCalc.GetExpectedPolarity(pulse)
             
-          if result.IsBaseline() == 0 and pulse.GetIsHeatPulse() == False:
+          if pulse.GetIsHeatPulse() == False:
             if polarity*result.GetAmp() < maxAmp: 
-              maxAmp = math.fabs(result.GetAmp())
+              maxAmp = polarity*result.GetAmp()
               pulseWithMaxAmp = k
-        
+
+        if pulseWithMaxAmp == -1:
+          #print 'no maximum amp found'
+          break  #we didn't find a pulse with a maximum amplitude. was it because of events with zero pulse length?
+
         pulse = bolo.GetPulseRecord(pulseWithMaxAmp)
         result = pulse.GetPulseAnalysisRecord(resultName)
         
         if result.GetPeakPosition() < pulse.GetPretriggerSize()*0.9:
           continue  #if the pulse position is less than the pretrigger size, let's assume this is noise, continuing to the next bolo record
-          
-        timeRelativeToTrigger = (result.GetPeakPosition() -  pulse.GetPretriggerSize())*pulse.GetPulseTimeWidth() 
-        peakPositionOfMax = result.GetPeakPosition()
         
+      
+        ionPulseTime = (result.GetPeakPosition() -  pulse.GetPretriggerSize())*pulse.GetPulseTimeWidth() 
+        peakPositionOfIon = result.GetPeakPosition()
+        
+        #print 'max peak position' peakPositionOfIon, ionPulseTime
         
         #loop back through the pulses to gather the results, requiring that the pulse peak position be within a 
-        #reasonable range (+- 3 bins in ionization and +- 1 bin in heat.)
+        #reasonable range (+- 10 bins in ionization and +- 3 bin in heat.)
         
         #first make sure that we have at least one "good" heat pulse
         goodHeatPulse = False
         for k in range(bolo.GetNumPulseRecords()):
-          if result.IsBaseline() == 0 and pulse.GetIsHeatPulse():
-            if result.GetPeakPostion() < peakPositionOfMax + 1 and result.GetPeakPosition() > peakPositionOfMax - 1:
+          pulse = bolo.GetPulseRecord(k)
+          if pulse.GetIsHeatPulse():
+            result = pulse.GetPulseAnalysisRecord(resultName)
+            polarity = polCalc.GetExpectedPolarity(pulse)
+            heatPulseTime = (result.GetPeakPosition() -  pulse.GetPretriggerSize())*pulse.GetPulseTimeWidth()
+            
+            if math.fabs(heatPulseTime-ionPulseTime) <  3.0*pulse.GetPulseTimeWidth():
+              print 'good heat pulse found', pulse.GetChannelName()
+              print heatPulseTime, ionPulseTime, math.fabs(heatPulseTime-ionPulseTime), '<', 3.0*pulse.GetPulseTimeWidth()
+              chanInfo = detectorInfo[bolo.GetDetectorName()]['chans'][pulse.GetChannelName()]
               chanInfo['hist'].Fill(polarity*result.GetAmp())
               goodHeatPulse = True
+
+
+              #fill the correlation histogram for the other channels
+              for kk in range(bolo.GetNumPulseRecords()):
+                if kk != k:
+                  otherPulse = bolo.GetPulseRecord(kk)
+                  otherPol = polCalc.GetExpectedPolarity(pulse)
+                  otherResult = otherPulse.GetPulseAnalysisRecord(resultName)
+                  
+                  chanInfo['corrhists'][otherPulse.GetChannelName()].Fill( polarity*result.GetAmp(), otherPol*otherResult.GetAmp())
+
           
         if goodHeatPulse == False:
-          continue  # move on to the bolometer event
+          continue  # move on to the next bolometer event
               
+        
         for k in range(bolo.GetNumPulseRecords()):
           pulse = bolo.GetPulseRecord(k)
           sumIon = 0
           
-          chanInfo = detectorInfo[bolo->GetDetectorName()][pulse->GetChannelName()]
+          chanInfo = detectorInfo[bolo.GetDetectorName()]['chans'][pulse.GetChannelName()]
           result = pulse.GetPulseAnalysisRecord(resultName)
-          polarity = polCalc.GetExpectedPolarity(p)
+          polarity = polCalc.GetExpectedPolarity(pulse)
               
           #just focus on the ionization pulses here.
-          if result.IsBaseline() == 0 and pulse.GetIsHeatPulse() == False:
+          if pulse.GetIsHeatPulse() == False:
             
-            if result.GetPeakPostion() < peakPositionOfMax + 3 and result.GetPeakPosition() > peakPositionOfMax - 3:
+            if math.fabs(result.GetPeakPosition() - peakPositionOfIon) < 10.0:
+              print 'good ion pulse found', pulse.GetChannelName()
+              print result.GetPeakPosition(), peakPositionOfIon, math.fabs(result.GetPeakPosition() - peakPositionOfIon), '<', 10.0
               chanInfo['hist'].Fill(polarity*result.GetAmp())  
               sumIon += polarity*result.GetAmp()
               
@@ -170,15 +222,22 @@ def main(arg):
               for kk in range(bolo.GetNumPulseRecords()):
                 if kk != k:
                   otherPulse = bolo.GetPulseRecord(kk)
-                  otherPol = polCalc.GetExpectedPolarity(p)
-                  otherResult = otherPulse.GetPulseAnalyaisRecord(resultName)
+                  otherPol = polCalc.GetExpectedPolarity(pulse)
+                  otherResult = otherPulse.GetPulseAnalysisRecord(resultName)
                   
                   chanInfo['corrhists'][otherPulse.GetChannelName()].Fill( polarity*result.GetAmp(), otherPol*otherResult.GetAmp())
           
           
               
-          
+                  
         detectorInfo[det]['sumIonHist'].Fill(sumIon)  
-      
+
+  fout.cd()
+  for i in range(len(histList)):
+    if histList[i]!=None:
+      print 'writing hist', histList[i].GetName(), histList[i].GetEntries()
+      histList[i].Write()
+  fout.Close()
+
 if __name__ == '__main__':
   main(*sys.argv[1:])
